dd0ca142e043cff8856649046c496ac93c2312cb
Gabriel L Berlot Peralta <gabriel.berlot-peralta@dxc.com>
2020-04-27 15:12:58 -0300

    fix count issue

--- terraform/app_name/prod/main.tf
+++ terraform/app_name/prod/main.tf
@@ -5,18 +5,18 @@ provider "aws" {
 
 module "my_vpc" {
   source      = "../modules/vpc"
-  ec2_count     = 1
   vpc_cidr    = "192.168.0.0/16"
   vpc_id      = "${module.my_vpc.vpc_id}"
   subnet1_cidr = "192.168.1.0/24"
   subnet2_cidr = "192.168.2.0/24"
 }
 
-module "my_ec2_jenkins" {
+module "my_ec2" {
   source        = "../modules/ec2"
   subnet_id     = "${module.my_vpc.terra_public_subnet1_id}"
   ami_id        = "ami-085925f297f89fce1"
   instance_type = "t2.medium"
+  ec2_count     = 1
   vpc_security_group_ids = ["${module.my_vpc.terra_sg_ssh_22_id}"]
   
 }
