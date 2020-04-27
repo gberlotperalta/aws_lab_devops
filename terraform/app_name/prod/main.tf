58cb356c3b293a487f570b4039ced156663b659d
Gabriel L Berlot Peralta <gabriel.berlot-peralta@dxc.com>
2020-04-27 11:05:49 -0300

    fix issues in vpc module

--- terraform/app_name/prod/main.tf
+++ terraform/app_name/prod/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "my_vpc" {
   source      = "../modules/vpc"
   vpc_cidr    = "192.168.0.0/16"
-  vpc_id      = "${module.my_vpc.id}"
+  vpc_id      = "${module.my_vpc.vpc_id}"
   subnet1_cidr = "192.168.1.0/24"
   subnet2_cidr = "192.168.2.0/24"
 }
