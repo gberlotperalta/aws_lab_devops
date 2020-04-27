1a307bf51299f41f4581e450b0bce75ff37f0c30
Gabriel L Berlot Peralta <gabriel.berlot-peralta@dxc.com>
2020-04-27 11:07:28 -0300

    fix issues in ec2

--- terraform/app_name/prod/main.tf
+++ terraform/app_name/prod/main.tf
@@ -13,7 +13,7 @@ module "my_vpc" {
 
 module "my_ec2" {
   source        = "../modules/ec2"
-  vpc_id      = "${module.my_vpc.id}"
+  vpc_id      = "${module.my_vpc.vpc_id}"
   subnet_id     = "${module.my_vpc.subnet_id}"
   ami_id        = "ami-5a8da735"
   instance_type = "t2.micro"
