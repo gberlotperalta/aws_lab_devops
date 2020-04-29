5b397f6155f0bc0dae84fb764fbadb25bf3d10ae
Gabriel L Berlot Peralta <gabriel.berlot-peralta@dxc.com>
2020-04-29 00:51:30 -0300

    chnage variable name

--- terraform/app_name/prod/main.tf
+++ terraform/app_name/prod/main.tf
@@ -7,8 +7,8 @@ module "vpc" {
   source      = "../modules/vpc"
   vpc_cidr    = "10.0.0.0/16"
   vpc_id      = "${module.vpc.terra_vpc_id}"
-  subnet1_cidr = "10.0.1.0/24"
-  subnet2_cidr = "10.0.2.0/24"
+  subnet_pub_cidr = "10.0.1.0/24"
+  subnet_pri_cidr = "10.0.2.0/24"
 }
 
 module "ec2_jenkins" {
