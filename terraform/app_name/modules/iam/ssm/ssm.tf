7eb5de55ecac46b8cd141a899776ffa59006c8eb
Gabriel L Berlot Peralta <gabriel.berlot-peralta@dxc.com>
2020-05-04 18:25:09 -0300

    test

--- terraform/app_name/modules/iam/ssm/ssm.tf
+++ terraform/app_name/modules/iam/ssm/ssm.tf
@@ -6,7 +6,7 @@ resource "aws_iam_role_policy" "terra_ssm_policy" {
 
 resource "aws_iam_role" "terra_ssm_role" {
   name = "terra_ssm_role"
-  assume_role_policy = "${var.assume_role_policy}"
+  assume_role_policy = "${file(assume_role_policy.json)}"
 }
 
 resource "aws_iam_instance_profile" "terra_ssm_profile" {
