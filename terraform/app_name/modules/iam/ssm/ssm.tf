52030720100126616ed931c6136ac7041cc46b42
Gabriel L Berlot Peralta <gabriel.berlot-peralta@dxc.com>
2020-05-05 01:30:20 -0300

    add template_files

--- terraform/app_name/modules/iam/ssm/ssm.tf
+++ terraform/app_name/modules/iam/ssm/ssm.tf
@@ -1,12 +1,20 @@
+data "template_file" "policy" {
+  template = "${file("${path.module}/policy.json")}"
+}
+
 resource "aws_iam_role_policy" "terra_ssm_policy" {
   name = "terra_ssm_policy"
   role = "${aws_iam_role.terra_ssm_role.id}"
-  policy = file("policy.json")
+  policy = "${data.template_file.policy.rendered}"
+}
+
+data "template_file" "assume-role-policy" {
+  template = "${file("${path.module}/assume_role_policy.json")}"
 }
 
 resource "aws_iam_role" "terra_ssm_role" {
   name = "terra_ssm_role"
-  assume_role_policy = file("assume_role_policy.json")
+  assume_role_policy = "${data.template_file.assume-role-policy.rendered}"
 }
 
 resource "aws_iam_instance_profile" "terra_ssm_profile" {
